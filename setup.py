b3f057a91468f71ff630c65d22d82d2ed7b4eb28
anhduc1210 <anhduc12101992@gmail.com>
2018-04-26 22:18:15 +0900

    major changes

--- setup.py
+++ setup.py
@@ -1,33 +1,62 @@
 from setuptools import setup, find_packages
-import codecs
 import os
 
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+AUTHOR = 'DUC NGUYEN'
+MAJOR = 0
+MINOR = 0
+MICRO = '2a'
+VERSION = '%d.%d.%s' % (MAJOR, MINOR, MICRO)
 
-setup(
-    name='neuralnet-pytorch',
-    version='0.0.1a',
-    description='A high-level library on top of Pytorch.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/justanhduc/neuralnet-pytorch',
-    author='Duc Nguyen',
-    author_email='adnguyen@yonsei.ac.kr',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5'
-    ],
-    packages=find_packages(exclude=['examples']),
-    install_requires=['matplotlib', 'scipy', 'numpy'],
-    project_urls={
-        'Bug Reports': 'https://github.com/justanhduc/neuralnet-pytorch/issues',
-        'Source': 'https://github.com/justanhduc/neuralnet-pytorch/',
-    },
-)
+
+def write_version_py(filename='neuralnet_pytorch/version.py'):
+    cnt = """
+#THIS FILE IS GENRERATED FROM SETUP.PY
+
+version = '%(version)s'
+author = '%(author)s'
+"""
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'author': AUTHOR})
+    finally:
+        a.close()
+
+
+def setup_package():
+    write_version_py()
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+    setup(
+        name='neuralnet-pytorch',
+        version=VERSION,
+        description='A high-level library on top of Pytorch.',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/justanhduc/neuralnet-pytorch',
+        author='Duc Nguyen',
+        author_email='adnguyen@yonsei.ac.kr',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+            'Operating System :: Microsoft :: Windows :: Windows 10',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.5'
+        ],
+        platforms=['Windows', 'Linux'],
+        packages=find_packages(exclude=['examples']),
+        install_requires=['theano', 'matplotlib', 'scipy', 'numpy'],
+        project_urls={
+            'Bug Reports': 'https://github.com/justanhduc/neuralnet-pytorch/issues',
+            'Source': 'https://github.com/justanhduc/neuralnet-pytorch',
+        },
+    )
+
+
+if __name__ == '__main__':
+    setup_package()
