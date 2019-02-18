525b01eaf34b7e3a4748edf63807f1d36a78103c
Duc Nguyen <anhduc12101992@gmail.com>
2019-02-18 22:08:13 +0900

    resolve conflicts in setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ def setup_package():
         cmdclass={
             'build_ext': BuildExtension
         },
-        install_requires=['visdom', 'matplotlib', 'scipy', 'numpy'],
+        install_requires=['visdom', 'matplotlib', 'scipy', 'numpy', 'tensorboardX'],
         project_urls={
             'Bug Reports': 'https://github.com/justanhduc/neuralnet-pytorch/issues',
             'Source': 'https://github.com/justanhduc/neuralnet-pytorch',
