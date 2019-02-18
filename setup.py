9215962bc3271ae21b3e076559b2262680614307
Duc Nguyen <anhduc12101992@gmail.com>
2019-02-18 22:08:13 +0900

    added tensorboardX to requirements

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def setup_package():
         ],
         platforms=['Windows', 'Linux'],
         packages=find_packages(exclude=['examples']),
-        install_requires=['visdom', 'matplotlib', 'scipy', 'numpy'],
+        install_requires=['visdom', 'matplotlib', 'scipy', 'numpy', 'tensorboardX'],
         project_urls={
             'Bug Reports': 'https://github.com/justanhduc/neuralnet-pytorch/issues',
             'Source': 'https://github.com/justanhduc/neuralnet-pytorch',
