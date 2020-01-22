869a152fb7511d211d44eb7d74470b8664260904
Duc Nguyen <anhduc12101992@gmail.com>
2020-01-22 21:31:21 +0900

    reformated C++/CUDA codes based on clang-format

--- neuralnet_pytorch/extensions/include/chamfer_cuda.h
+++ neuralnet_pytorch/extensions/include/chamfer_cuda.h
@@ -2,6 +2,8 @@
 #include <torch/torch.h>
 #include <vector>
 
-
 std::vector<torch::Tensor> chamfer_forward(at::Tensor xyz1, at::Tensor xyz2);
-std::vector<torch::Tensor> chamfer_backward(at::Tensor xyz1, at::Tensor xyz2, at::Tensor graddist1, at::Tensor graddist2, at::Tensor idx1, at::Tensor idx2);
+std::vector<torch::Tensor> chamfer_backward(at::Tensor xyz1, at::Tensor xyz2,
+                                            at::Tensor graddist1,
+                                            at::Tensor graddist2,
+                                            at::Tensor idx1, at::Tensor idx2);
