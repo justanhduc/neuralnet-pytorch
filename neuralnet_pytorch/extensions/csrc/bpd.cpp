3deaa1cef1e8e8fc3f79e516cec9d662c07a3561
Duc <anhduc12101992@gmail.com>
2020-10-13 15:34:47 +0900

    fixed a bug in bpd c++

--- neuralnet_pytorch/extensions/csrc/bpd.cpp
+++ neuralnet_pytorch/extensions/csrc/bpd.cpp
@@ -6,7 +6,7 @@ torch::Tensor
 batch_pairwise_distance_forward(torch::Tensor x, torch::Tensor y) {
     auto xx = at::sum(at::pow(x, 2), -1);
     auto yy = at::sum(at::pow(y, 2), -1);
-    auto xy = at::bmm(x, at::transpose(y, -1, -2));
+    auto xy = at::matmul(x, at::transpose(y, -1, -2));
 
     auto rx = xx.unsqueeze(-2).expand_as(at::transpose(xy, -1, -2));
     auto ry = yy.unsqueeze(-2).expand_as(xy);
